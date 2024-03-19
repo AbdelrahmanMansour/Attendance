5db70d214e1b8e3d45db4fccad9bf56d978fbc4d
Nabmikalg <131638930+Nabmikalg@users.noreply.github.com>
2024-03-19 12:59:04 -0500

    Adding implementation for the advanced cases.

--- Attendance/src/main/java/com/system/Attendance/controller/ScannerController.java
+++ Attendance/src/main/java/com/system/Attendance/controller/ScannerController.java
@@ -3,6 +3,7 @@ package com.system.Attendance.controller;
 import com.system.Attendance.domain.Scanner;
 import com.system.Attendance.service.contract.ScannerPayload;
 import edu.miu.common.controller.BaseReadWriteController;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/scanners")
 public class ScannerController extends BaseReadWriteController<ScannerPayload, Scanner, Integer> {
 
+    @PostMapping("/{scannerCode}/records")
+    public void createMemberSessionRecord() {
+        //
+        System.out.println("Scanning");
+    }
 }
