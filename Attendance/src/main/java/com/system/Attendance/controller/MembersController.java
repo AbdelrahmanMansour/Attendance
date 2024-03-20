28d9459503bef9231f4f283a60b70b7d653ca173
Deo_Mugabe <79472484+DegraBest@users.noreply.github.com>
2024-03-20 01:33:31 -0500

    added advanced 5 and test

--- Attendance/src/main/java/com/system/Attendance/controller/MembersController.java
+++ Attendance/src/main/java/com/system/Attendance/controller/MembersController.java
@@ -1,8 +1,14 @@
 package com.system.Attendance.controller;
 
 import com.system.Attendance.domain.Member;
+import com.system.Attendance.service.MembersService;
 import com.system.Attendance.service.contract.MembersPayload;
 import edu.miu.common.controller.BaseReadWriteController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/members")
 public class MembersController extends BaseReadWriteController<MembersPayload, Member, Integer> {
 
+    @Autowired
+    private MembersService memberService;
+    @GetMapping("/{memberId}/events/{eventId}/attendance")
+    public ResponseEntity<Integer> countAttendanceForEventByMember(@PathVariable("memberId") Integer memberId,
+                                                                   @PathVariable("eventId") Long eventId){
+
+        int attendanceCount = memberService.countAttendanceForEventByMember(memberId, eventId);
+        return new ResponseEntity<>(attendanceCount, HttpStatus.OK);
+    }
 }
