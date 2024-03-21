8d04e528555f93f81bb6413a7cd493eaec5bf7ee
vien <viennv@newbees.jp>
2024-03-20 23:14:54 -0500

    update fix change

--- Attendance/src/main/java/com/system/Attendance/domain/Scanner.java
+++ Attendance/src/main/java/com/system/Attendance/domain/Scanner.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,13 +20,17 @@ public class Scanner implements Serializable {
     private Location location;
 
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "session_id")
-    private List<Session> sessions;
+    @JoinColumn(name = "scanner_id")
+    private List<Session> sessions = new ArrayList<>();
 
     public Scanner() {
     }
     public Scanner(Location location) {
         this.location = location;
+    }
+
+    public void addSession(Session session) {
+        this.sessions.add(session);
     }
 
 }
